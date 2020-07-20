ae2a877ed67c6ed54aca15eb0f18231a2a6bb1e6
gabrielcfvg <stalin@PC.localdomain>
2020-07-20 07:32:52 -0300

    adicionado suporte a redimencionamento de tela durante o uso do promt

--- src/editor_src/editor_prompts.rs
+++ src/editor_src/editor_prompts.rs
@@ -26,52 +26,71 @@ impl Editor {
 
         loop {
 
-            if let Ok(Event::Key(key_event)) = read() {
+            match read().unwrap() {
 
-                match key_event {
+                Event::Key(key_event) => {
+                
+                    match key_event {
 
-                    KeyEvent{code: KeyCode::Esc, modifiers: _} => {
-                        return None;
-                    }
-
-                    KeyEvent{code: KeyCode::Enter, modifiers: _} => {
-                        break;
-                    }
-
-                    KeyEvent{code: KeyCode::Left, modifiers: _} => {
-                        if cursor != 0 {
-                            cursor -= 1;
+                        KeyEvent{code: KeyCode::Esc, modifiers: _} => {
+                            return None;
                         }
-                    }
-                    KeyEvent{code: KeyCode::Right, modifiers: _} => {
-                        if cursor < text.len() {
+
+                        KeyEvent{code: KeyCode::Enter, modifiers: _} => {
+                            break;
+                        }
+
+                        KeyEvent{code: KeyCode::Left, modifiers: _} => {
+                            if cursor != 0 {
+                                cursor -= 1;
+                            }
+                        }
+                        KeyEvent{code: KeyCode::Right, modifiers: _} => {
+                            if cursor < text.len() {
+                                cursor += 1;
+                            }
+                        }
+
+                        KeyEvent{code: KeyCode::Char(ch), modifiers: _} => {
+                            if ch.is_ascii() {
+                                text.insert(cursor, ch);
+                            }
                             cursor += 1;
                         }
-                    }
 
-                    KeyEvent{code: KeyCode::Char(ch), modifiers: _} => {
-                        if ch.is_ascii() {
-                            text.insert(cursor, ch);
+                        KeyEvent{code: KeyCode::Backspace, modifiers: _} => {
+                            if cursor > 0 {
+                                text.remove(cursor-1);
+                                cursor -= 1;
+                            }
                         }
-                        cursor += 1;
-                    }
 
-                    KeyEvent{code: KeyCode::Backspace, modifiers: _} => {
-                        if cursor > 0 {
-                            text.remove(cursor-1);
-                            cursor -= 1;
-                        }
+                        _ => ()
                     }
+                },
 
-                    _ => ()
-                }
+                Event::Resize(x, y) => {
+                    self.number_row = (y-2) as usize;
+                    self.number_col = x as usize;
+
+                    self.render_screen(true);
+                    self.update_bar();
+
+                },
+                _ => ()
             }
+
+            let mut saida = format!("{}: {}", message, text);
+            if saida.len() > self.number_col {
+                saida = saida[..self.number_col].to_string();
+            }
+
 
             execute!(
                 stdout(),
                 MoveTo(0, (self.number_row + 1) as u16),
                 Clear(ClearType::CurrentLine),
-                Print(format!("{}: {}", message, text)),
+                Print(saida),
                 MoveTo((message.len() + 2 + cursor) as u16, (self.number_row+1) as u16)
             ).unwrap();
 
